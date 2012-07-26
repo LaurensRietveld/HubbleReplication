fcda11802791708adb4fa226e172398db5dc9473
Laurens Rietveld <l.j.rietveld@vu.nl>
2012-07-26 12:01:57 +0200

    initial implementation of local replication daemon

---
+++ replicationDaemon/src/main/java/com/data2semantics/replication/localDaemon/LocalDaemon.java
@@ -0,0 +1,13 @@
+package com.data2semantics.replication.localDaemon;
+
+/**
+ * Hello world!
+ *
+ */
+public class LocalDaemon 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
